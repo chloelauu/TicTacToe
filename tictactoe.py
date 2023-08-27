3d5f632b0ba2c0ed6a797fe625df8caf1a3f09fd
chlv33 <mabinogigames7@gmail.com>
2023-08-27 21:38:51 +0800

    u2

--- tictactoe.py
+++ tictactoe.py
@@ -25,11 +25,69 @@ def playerInput(board):
 
 # check for win / tie
 
+# check for horizontal lines
+def checkHorizontal(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+    
+# check for vertical lines
+def checkVertic(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+    
+# check for diagonal lines
+def checkDiag(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        winner = board[2]
+        return True
+    
+def checkTie(board):
+    global gameRunning
+    if "-" not in board:
+        printBoard(board)
+        print("It is a tie! End of game.")
+        gameRunning = False
+
+def checkWin(board):
+    global gameRunning
+    if checkDiag(board) or checkHorizontal(board) or checkVertic(board):
+        print(f"The winner is {winner}")
+        gameRunning = False
+    
 # switch between player 1 & 2
+def SwitchPlayer():
+    global currentPlayer
+    if currentPlayer == "X":
+        currentPlayer = "O"
+    else:
+        currentPlayer = "X"
 
 # check for win / tie again
 
 while gameRunning:
     printBoard(board)
     playerInput(board)
+    checkWin()
+    checkTie(board)
+    SwitchPlayer()
 
