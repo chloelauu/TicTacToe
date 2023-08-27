1de0e27abff8b257035ee02c1eb7ed7ea7cc9acd
chlv33 <mabinogigames7@gmail.com>
2023-08-27 20:44:35 +0800

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,23 @@
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+currentPlayer = "X"
+winner = None
+gameRunning = true
+
+# print board for tic tac toe game
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " |  " + board[2])
+    print(board[3] + " | " + board[4] + " |  " + board[5])
+    print(board[6] + " | " + board[7] + " |  " + board[8])
+printBoard(board)
+
+# take player input
+
+# check for win / tie
+
+# switch between player 1 & 2
+
+# check for win / tie again
+
