ab0cc148f43848f7ce27011c068b762b4e22b06f
chlv33 <mabinogigames7@gmail.com>
2023-08-27 20:47:13 +0800

    rr

--- tictactoe.py
+++ tictactoe.py
@@ -4,13 +4,13 @@ board = ["-", "-", "-",
 
 currentPlayer = "X"
 winner = None
-gameRunning = true
+gameRunning = True
 
 # print board for tic tac toe game
 def printBoard(board):
-    print(board[0] + " | " + board[1] + " |  " + board[2])
-    print(board[3] + " | " + board[4] + " |  " + board[5])
-    print(board[6] + " | " + board[7] + " |  " + board[8])
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
 printBoard(board)
 
 # take player input
